02e13f2857d0afe66cd6391588a6dd5e6295f0b0
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2023-05-22 16:05:30 -0300

    DTO com Java Record.

---
+++ api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,7 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.DadosEsdereco;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEsdereco endereco) {
+
+}

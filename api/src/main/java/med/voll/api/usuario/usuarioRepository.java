3a64e8b5be447c02925faf96bd315a005c59fe77
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2023-05-29 15:33:00 -0300

    Repository e Service.

--- api/src/main/java/med/voll/api/usuario/usuarioRepository.java
+++ api/src/main/java/med/voll/api/usuario/usuarioRepository.java
@@ -1,5 +1,9 @@
 package med.voll.api.usuario;
 
-public interface usuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
+public interface usuarioRepository extends JpaRepository<Usuario, Long> {
+
+	UserDetails findByLogin(String login);
 }

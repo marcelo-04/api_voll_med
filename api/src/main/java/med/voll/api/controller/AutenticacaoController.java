2e361fec0b136ce674e2d80a672e587d9ea6feb4
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2023-05-30 16:50:39 -0300

    Controller de autenticação.

--- api/src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ api/src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +16,15 @@ import med.voll.api.usuario.DadosAutenticacao;
 @RequestMapping("/login")
 public class AutenticacaoController {
 	
+	@Autowired
+	private AuthenticationManager manager;
+	
 	@PostMapping
 	public ResponseEntity efetuarlogin(@RequestBody @Valid DadosAutenticacao dados) {
 		
-		return null;
+		var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+		var authentication = manager.authenticate(token);
 		
+		return ResponseEntity.ok().build();		
 	}
-
 }

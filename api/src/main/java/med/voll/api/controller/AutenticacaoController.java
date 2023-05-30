affb806e4658ae45a538866c9713cbb435533739
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2023-05-30 16:08:26 -0300

    Configuração de segurança.

---
+++ api/src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -0,0 +1,23 @@
+package med.voll.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import med.voll.api.usuario.DadosAutenticacao;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+	
+	@PostMapping
+	public ResponseEntity efetuarlogin(@RequestBody @Valid DadosAutenticacao dados) {
+		
+		return null;
+		
+	}
+
+}

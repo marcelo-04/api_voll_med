4f504f457707320a103219868c88c4dd471f6ff3
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2023-05-31 11:07:55 -0300

    Ajustes na geração do token.

--- api/src/main/java/med/voll/api/infra/security/TokenService.java
+++ api/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
@@ -15,10 +16,12 @@ import med.voll.api.usuario.Usuario;
 @Service
 public class TokenService {
 	
+	@Value("${api.security.token.secret}")
+	private String secret;
+	
 	public String gerarToken(Usuario usuario) {
-		
 		try {
-		    var algoritimo = Algorithm.HMAC256("12345678");
+		    var algoritimo = Algorithm.HMAC256(secret);
 		    return JWT.create()
 		        .withIssuer("API Voll.med")
 		        .withSubject(usuario.getLogin())
